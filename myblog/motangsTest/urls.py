3aa6fe8234b38a9e4cbbe39793a830ac843ab18a
吴亚雄 <1628967341@qq.com>
2020-01-02 10:55:44 +0800

    blog_wuyaxiong

--- myblog/motangsTest/urls.py
+++ myblog/motangsTest/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import index
+from . import views
 
+app_name = 'blog_wuyaxiong'
 urlpatterns = [
     path('index/',index),
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('MoTangS/',include('motangsTest.urls')),
+    path('motangsTest/',include('motangsTest.urls')),
+   
 ]
